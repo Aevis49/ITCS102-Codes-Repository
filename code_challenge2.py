4b29b2b942049e9e4d95be1642d5b02830c0004b
Aevis49 <jpbuela94@gmail.com>
2026-08-25 12:54:36 +0800

    Add files via upload

---
+++ code_challenge2.py
@@ -0,0 +1,39 @@
+money = 7339
+onethousand = 1000
+fivehundred = 500
+twohundred = 200
+onehundred = 100
+fifty = 50
+twenty = 20
+ten = 10
+five = 5
+one = 1
+print("Money to deposit -->", money)
+print("Money needed to achieve 7339 is ", money//onethousand, "One Thousand Pesos")
+
+money = 339
+print("Money needed to achieve 7339 is ", money//fivehundred, "Five Hundred Pesos")
+
+money = 339
+print("Money needed to achieve 7339 is ", money//twohundred, "Two Hundred Pesos")
+
+money = 139
+print("Money needed to achieve 7339 is ", money//onehundred, "One Hundred Pesos")
+
+money = 39
+print("Money needed to achieve 7339 is ", money//fifty, "Fifty Pesos")
+
+money = 39
+print("Money needed to achieve 7339 is ", money//twenty, "Twenty Pesos")
+
+money = 19
+print("Money needed to achieve 7339 is ", money//ten, "Ten Pesos")
+
+money = 9
+print("Money needed to achieve 7339 is ", money//five, "Five Pesos")
+
+money = 4
+print("Money needed to achieve 7339 is ", money//one, "One Pesos")
+
+money_inhand = 7339
+print("You have deposited --> ", money_inhand)
