33429f84eeb01832ee093273cd6799ec3447f49b
Aevis49 <jpbuela94@gmail.com>
2026-08-26 19:30:19 +0800

    Refactor money breakdown calculation
    Refactored code to calculate and print the breakdown of money in various denominations using modulus operations.

--- code_challenge2.py
+++ code_challenge2.py
@@ -1,39 +1,30 @@
-money = 7339
-onethousand = 1000
-fivehundred = 500
-twohundred = 200
-onehundred = 100
-fifty = 50
-twenty = 20
-ten = 10
-five = 5
-one = 1
-print("Money to deposit -->", money)
-print("Money needed to achieve 7339 is ", money//onethousand, "One Thousand Pesos")
+money = 7449
+a = money//1000 
+rem = money %1000 #sheesh it actually worked, modulus is key?
+b = rem//500
+rem = money %500
+c = rem//200
+rem = money %200
+d = rem//100
+rem = money %100
+e = rem//50
+rem = money %50
+f = rem//20
+rem = money %20
+g = rem//10
+rem = money %10
+h = rem//5
+rem = money %5
+i = rem//1
 
-money = 339
-print("Money needed to achieve 7339 is ", money//fivehundred, "Five Hundred Pesos")
-
-money = 339
-print("Money needed to achieve 7339 is ", money//twohundred, "Two Hundred Pesos")
-
-money = 139
-print("Money needed to achieve 7339 is ", money//onehundred, "One Hundred Pesos")
-
-money = 39
-print("Money needed to achieve 7339 is ", money//fifty, "Fifty Pesos")
-
-money = 39
-print("Money needed to achieve 7339 is ", money//twenty, "Twenty Pesos")
-
-money = 19
-print("Money needed to achieve 7339 is ", money//ten, "Ten Pesos")
-
-money = 9
-print("Money needed to achieve 7339 is ", money//five, "Five Pesos")
-
-money = 4
-print("Money needed to achieve 7339 is ", money//one, "One Pesos")
-
-money_inhand = 7339
-print("You have deposited --> ", money_inhand)
+print("Money to deposit -->", money ,"pesos")
+print("1000 - ", a)
+print("500 - ", b)
+print("200 - ", c)
+print("100 - ", d)
+print("50 - ", e)
+print("20 - ", f)
+print("10 - ", g)
+print("5 - ", h)
+print("1 - ", i)
+print("Congratulations! You received a total of --> ", money ,"pesos")
