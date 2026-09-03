20ac29093015639c6ab6680d05ea44fa53af20f8
Aevis49 <jpbuela94@gmail.com>
2026-09-03 11:51:12 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,25 @@
+#Operators = Symbols 
+#+ - addition
+#- - subtraction
+#* - multiplication
+#/ - division
+#// - floor div
+#** - exponential
+#% - modulus
+
+A = 94
+B = 4
+sum = A + B
+sub = A - B
+mul = A*B
+
+
+
+
+print("The sum of ",A," and ",B," is ",sum)
+print("The subtraction of ",A," and ",B," is", sub)
+print("The multiplication of ",A," and ",B," is ", mul)
+print("The division of ",A," and ",B," is ", 94/4)
+print("The floor division of ",A," and ",B," is ", 94//4) #IT REMOVES THE REMAINDER, AMAZING
+print("The exponetial multiplication of ",A," and ",B," is ", 94**4)
+print("The modulus of ",A," and ",B," is ", 94%4)
