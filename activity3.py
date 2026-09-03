4bd9d40cddbd0ec3f7e02f18cf6b5e84fab20725
Aevis49 <jpbuela94@gmail.com>
2026-09-03 11:50:45 +0800

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,29 @@
+#Integer Variable
+soldToday = 20
+totalBranches = 4
+cash = 999999
+
+#Float Variable
+tax = 4.5
+height = 165.9
+temperature = 26.5
+
+#String Variable
+product = "Vegetables"
+productstatus = "Great"
+seller = "Jp"
+interceptorName = "Aevis"
+
+#Boolean variable
+hasPermit = True
+needMoney = True
+notTired = False
+isZenos = False
+
+print(interceptorName, cash)
+
+print("Hello my name is", seller, "and I'm selling", productstatus, product)
+print(interceptorName, interceptorName,interceptorName,"Who's mind will you inhabit next")
+print("The tax included is", tax , "and I hold", cash, "Pesos")
+print(isZenos)
+print(tax + cash - temperature)
