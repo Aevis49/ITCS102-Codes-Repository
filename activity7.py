a0c75b66d0776959db1ee1483937f26abf9ddf3a
Aevis49 <jpbuela94@gmail.com>
2026-09-03 12:29:05 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,31 @@
+total = 0
+
+n = eval(input("Enter a number --> "))
+total += n
+print("The number will be added")
+n1 = eval(input("Enter a number --> "))
+total -= n1
+print("The number will be subtracted")
+n2 = eval(input("Enter a number --> "))
+total += n2
+print("The number will be added")
+n3 = eval(input("Enter a number --> "))
+total += n3
+print("The number will be added")
+n4 = eval(input("Enter a number --> "))
+total *= n4
+print("The number will be multiplied")
+n5 = eval(input("Enter a number --> "))
+total *= n5
+print("The number will be multiplied")
+n6 = eval(input("Enter a number --> "))
+total += n6
+print("The number will be added")
+n7 = eval(input("Enter a number --> "))
+total = total /2
+print("The number will be divided")
+
+
+#Added an addition to those who want to see the decimals ^-^
+print("The integer: ",int(total))
+print("The float: ",float(total))
