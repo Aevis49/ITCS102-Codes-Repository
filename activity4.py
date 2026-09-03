9c44e452c2f551c04b73bd73a29cc3c4db9c3db4
Aevis49 <jpbuela94@gmail.com>
2026-09-03 11:51:02 +0800

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,15 @@
+#Escape sequence
+#\n = new line
+#\t = tab space
+#\r = carriage return
+#\\ = add a backslash
+#\* 
+#\b = backslash
+
+motto1 = "Never gonna give you up" 
+motto2 = "Never gonna \nlet you down"
+
+print(motto1)
+print(motto2)
+
+reborn = "T3RR4 \R4WRRRR"
