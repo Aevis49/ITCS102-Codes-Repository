04bf331314b9628eaf90ab8d3ecbc680b0f76439
Aevis49 <jpbuela94@gmail.com>
2026-09-08 12:40:22 +0800

    Add files via upload

---
+++ activity11.py
@@ -0,0 +1,14 @@
+username = "Elysia093"
+password = "lovefortheworld"
+
+u = input("Input Username --> ")
+p = input("Input Password --> ")
+
+if username == u and p == password:
+	print("ACCESS GRANTED")
+	print("Welcome, cutie~")
+else:
+	print("ACCESS DENIED")
+	print("You shouldn't be here")
+
+ 
---
+++ activity12.py
@@ -0,0 +1,15 @@
+import getpass
+username = "Elysia093"
+password = "lovefortheworld"
+
+u = input("Input Username --> ")
+p = getpass.getpass("Input Password --> ")
+
+if username == u and p == password:
+	print("ACCESS GRANTED")
+	print("Welcome, cutie~")
+else:
+	print("ACCESS DENIED")
+	print("You shouldn't be here")
+
+ 
