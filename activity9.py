e370122c6b01c5ea631d5db3d1b2e7099af6a440
Aevis49 <jpbuela94@gmail.com>
2026-09-08 11:52:42 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,24 @@
+#Relational operator demo
+
+x = 5
+y = 10
+
+text1 = 'Sheesh'
+text2 = 'sheesh'
+
+#1
+print(x > y)
+#2
+print(x < y)
+
+x = 5
+y = 5
+
+#3
+print(x >=y)
+#4
+print(x <=y)
+#5
+print(text1 == text2)
+#6
+print(text1 != text2)
